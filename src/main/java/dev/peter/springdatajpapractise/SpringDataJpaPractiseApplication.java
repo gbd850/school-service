9dab94a97380dc65ca81a3dd715901a52b8cdac4
gbd850 <mmyszaki@gmail.com>
2023-10-25 20:36:56 +0200

    Initial commit
    Set up project

---
+++ src/main/java/dev/peter/springdatajpapractise/SpringDataJpaPractiseApplication.java
@@ -0,0 +1,13 @@
+package dev.peter.springdatajpapractise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaPractiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaPractiseApplication.class, args);
+	}
+
+}
