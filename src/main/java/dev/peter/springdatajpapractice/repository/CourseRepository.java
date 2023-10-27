08efc2854ce0abc645982f0ed9e2d2c3de950815
gbd850 <mmyszaki@gmail.com>
2023-10-27 22:02:26 +0200

    Example utilizing PagingAndSorting repository interface

--- src/main/java/dev/peter/springdatajpapractice/repository/CourseRepository.java
+++ src/main/java/dev/peter/springdatajpapractice/repository/CourseRepository.java
@@ -1,6 +1,10 @@
 package dev.peter.springdatajpapractice.repository;
 
 import dev.peter.springdatajpapractice.model.Course;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,4 +12,8 @@ import org.springframework.transaction.annotation.Transactional;
 @Repository
 @Transactional
 public interface CourseRepository extends JpaRepository<Course, Long> {
+    Page<Course> findByTitleContaining(String title, Pageable pageable);
+//    Example:
+//    PageRequest page = PageRequest.of(0, 3, Sort.by("title").descending());
+//    findByTitleContaining("partialTitle", page).getContent();
 }
