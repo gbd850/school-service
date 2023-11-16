7f10a95c961fa58877c99ea91a68f7086b2efde7
student <s95615@pollub.edu.pl>
2023-11-16 18:39:22 +0100

    Add POST integration test for StudentController

--- src/test/java/dev/peter/springdatajpapractice/controller/StudentControllerTest.java
+++ src/test/java/dev/peter/springdatajpapractice/controller/StudentControllerTest.java
@@ -1,8 +1,12 @@
 package dev.peter.springdatajpapractice.controller;
 
+import com.google.gson.Gson;
 import dev.peter.springdatajpapractice.SpringDataJpaPractiseApplication;
+import dev.peter.springdatajpapractice.dto.StudentRequestDto;
+import dev.peter.springdatajpapractice.model.Guardian;
 import dev.peter.springdatajpapractice.model.Student;
 import dev.peter.springdatajpapractice.repository.StudentRepository;
+import dev.peter.springdatajpapractice.service.StudentService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -20,8 +24,10 @@ import java.util.List;
 
 import static net.bytebuddy.matcher.ElementMatchers.is;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.contains;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
@@ -35,13 +41,12 @@ class StudentControllerTest {
     private MockMvc mvc;
 
     @MockBean
-    private StudentRepository studentRepository;
+    private StudentService studentService;
 
     @Test
     public void givenStudent_whenGetStudents_thenStatus200() throws Exception {
 
-
-        when(studentRepository.findAll()).thenReturn(List.of(Student.builder().id(1L).firstName("bob").build()));
+        when(studentService.getAllStudents()).thenReturn(List.of(Student.builder().id(1L).firstName("bob").build()));
 
         mvc.perform(get("/api/students")
                         .contentType(MediaType.APPLICATION_JSON))
@@ -49,6 +54,25 @@ class StudentControllerTest {
                 .andExpect(content()
                         .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$[0].firstName", is("bob")).hasJsonPath());
+    }
+
+    @Test
+    public void givenStudent_whenPostStudent_thenStatus201() throws Exception {
+
+        StudentRequestDto studentRequestDto = new StudentRequestDto("Jan",
+                "Kowalski",
+                "wp@wp.pl",
+                "G1",
+                "g1@g.com",
+                "123456789");
+
+        Gson gson = new Gson();
+
+        mvc.perform(post("/api/students")
+                        .content(gson.toJson(studentRequestDto))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated());
     }
   
 }
