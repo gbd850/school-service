7a89522429b0aab70d45e196b617b656ab330e86
student <s95615@pollub.edu.pl>
2023-11-23 19:47:40 +0100

    Fix NullPointerException in test

--- src/test/java/dev/peter/springdatajpapractice/controller/CourseControllerTest.java
+++ src/test/java/dev/peter/springdatajpapractice/controller/CourseControllerTest.java
@@ -5,6 +5,8 @@ import dev.peter.springdatajpapractice.model.Course;
 import dev.peter.springdatajpapractice.service.CourseService;
 import dev.peter.springdatajpapractice.service.StudentService;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
@@ -13,6 +15,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Optional;
@@ -28,6 +31,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @EnableAutoConfiguration(exclude= SecurityAutoConfiguration.class)
 @AutoConfigureTestDatabase
+@RunWith(SpringRunner.class)
 public class CourseControllerTest {
     @Autowired
     private MockMvc mvc;
@@ -38,14 +42,13 @@ public class CourseControllerTest {
     @Test
     public void givenCourdeId_whenCourse_thenStatus200() throws Exception {
 
-        Long id = 1L;
-        when(courseService.getCourseById(id)).thenReturn(Course.builder().id(1L).title("Course 1").credit(5).build());
+        when(courseService.getCourseById(1L)).thenReturn(Course.builder().id(1L).title("Course 1").credit(5).build());
 
         mvc.perform(get("/api/courses/1")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content()
                         .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$[0].title", is("Course 1")).hasJsonPath());
+                .andExpect(jsonPath("$.title", is("Course 1")).hasJsonPath());
     }
 }
