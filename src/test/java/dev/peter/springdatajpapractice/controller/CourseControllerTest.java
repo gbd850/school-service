1b09881fb42e74f664a1e6a060c0c94119a06ba0
student <s95615@pollub.edu.pl>
2023-11-23 18:32:53 +0100

    Fix annotations in Course test class

--- src/test/java/dev/peter/springdatajpapractice/controller/CourseControllerTest.java
+++ src/test/java/dev/peter/springdatajpapractice/controller/CourseControllerTest.java
@@ -1,10 +1,15 @@
 package dev.peter.springdatajpapractice.controller;
 
+import dev.peter.springdatajpapractice.SpringDataJpaPractiseApplication;
 import dev.peter.springdatajpapractice.model.Course;
 import dev.peter.springdatajpapractice.service.CourseService;
 import dev.peter.springdatajpapractice.service.StudentService;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
@@ -18,8 +23,12 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest
-class CourseControllerTest {
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+        classes = SpringDataJpaPractiseApplication.class)
+@AutoConfigureMockMvc
+@EnableAutoConfiguration(exclude= SecurityAutoConfiguration.class)
+@AutoConfigureTestDatabase
+public class CourseControllerTest {
 
     @Autowired
     private MockMvc mvc;
