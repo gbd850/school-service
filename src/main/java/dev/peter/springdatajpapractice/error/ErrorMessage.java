82acfcdc28cd0f241c5e6bd8b98ec874d86c3023
gbd850 <mmyszaki@gmail.com>
2023-12-05 16:50:50 +0100

    Add exception handling logic

---
+++ src/main/java/dev/peter/springdatajpapractice/error/ErrorMessage.java
@@ -0,0 +1,12 @@
+package dev.peter.springdatajpapractice.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorMessage {
+    private String message;
+}
