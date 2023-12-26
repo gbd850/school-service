c2554c66ae7722250200651a0f3313f026cf0ba4
gbd850 <mmyszaki@gmail.com>
2023-12-26 17:26:56 +0100

    Test Course repository

---
+++ src/test/java/dev/peter/springdatajpapractice/repository/CourseRepositoryTest.java
@@ -0,0 +1,45 @@
+package dev.peter.springdatajpapractice.repository;
+
+import dev.peter.springdatajpapractice.model.Course;
+import dev.peter.springdatajpapractice.model.CourseMaterial;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class CourseRepositoryTest {
+
+    @Autowired
+    private CourseRepository courseRepository;
+
+    @AfterEach
+    void tearDown() {
+        courseRepository.deleteAll();
+    }
+
+    @Test
+    void givenCourse_whenFindCourseByTitleFirstPage_thenReturnPageWithCourse() {
+        // given
+        String title = "title";
+        CourseMaterial courseMaterial = new CourseMaterial(1L, "url");
+        Course course = new Course(1L, title, 4, courseMaterial, null);
+        courseRepository.save(course);
+        // when
+        String partialTitle = "tit";
+        Page<Course> expected = courseRepository.findByTitleContaining(
+                partialTitle,
+                PageRequest.of(0, 3, Sort.by("title").descending())
+        );
+        // then
+        assertThat(expected).isNotEmpty()
+                .allMatch(c -> c.getTitle().contains(partialTitle))
+                .allMatch(c -> c.getTitle().equals(title));
+    }
+}
